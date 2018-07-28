0a3a4fb669d119580490d20c4b29ed342bf3c65b
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2018-07-28 12:14:10 -0300

    feat(models/university): Adds by_name scope to University which returns universities that contains given param in its name or all when param is nil perf(controllers/universities): Changes exposure to scope by name test(specs/models/university): Adds by_name scope tests test(specs/controllers/universities): Adds new tests against request with name param and improves existing tests

--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -1,7 +1,7 @@
 class UniversitiesController < ApplicationController
-  expose :universities, -> { University.all }
+  expose :universities, -> { University.by_name(params[:name]) }
 
   def index
-    render json: universities.as_json
+    render json: universities.as_json(only: [:id, :name])
   end
 end
--- app/models/university.rb
+++ app/models/university.rb
@@ -1,13 +1,13 @@
 class University < ApplicationRecord
-  validates_presence_of :name, :podio_id
-
   has_many :exchange_participants
+  
+  ##
+  # :singleton-method:
+  # Returns all Universities which contains in its name the given string 
+  # (case-insensitive)
+  # Will otherwise return all Universities if no param is given, due to
+  # LIKE's wildcard % (zero, one or multiple characters)
+  scope :by_name, -> (name = nil) { where("lower(name) LIKE ?", "%#{name}%") } 
 
-  def as_json
-    {
-      id: id,
-      name: name,
-      podio_id: podio_id
-    }
-  end
+  validates_presence_of :name, :podio_id
 end
