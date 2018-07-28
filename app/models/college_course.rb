e9c4759b9a31aeaab38ba2fd454204c393bddb25
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2018-07-28 12:22:05 -0300

    feat(models/college_course): Adds by_name scope to CollegeCourse which returns college_courses that contains given param in its name or all when param is nil test(specs/models/college_course): Adds by_name scope tests

--- app/models/college_course.rb
+++ app/models/college_course.rb
@@ -1,13 +1,13 @@
 class CollegeCourse < ApplicationRecord
-  validates_presence_of :name, :podio_id
-
   has_many :exchange_participants
 
-  def as_json
-    {
-      id: id,
-      name: name,
-      podio_id: podio_id
-    }
-  end
+  ##
+  # :singleton-method:
+  # Returns all College Courses which contains in its name the given string 
+  # (case-insensitive)
+  # Will otherwise return all College Courses if no param is given, due to
+  # LIKE's wildcard % (zero, one or multiple characters)
+  scope :by_name, -> (name = nil) { where("lower(name) LIKE ?", "%#{name}%") } 
+
+  validates_presence_of :name, :podio_id
 end
--- spec/models/college_course_spec.rb
+++ spec/models/college_course_spec.rb
@@ -1,6 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe CollegeCourse, type: :model do
+  describe "#associations" do
+    it { is_expected.to have_many :exchange_participants }
+  end
+
+  describe '#scopes' do
+    context 'by_name' do
+      let!(:college_course) { create(:college_course, name: 'Bachelor By Name Scope') }
+
+      let!(:college_courses) { create_list(:college_course, 3) }
+
+      context 'with no given param' do
+        it { expect(CollegeCourse.by_name).to match_array(CollegeCourse.all) }
+        it { expect(CollegeCourse.by_name.count). to eq CollegeCourse.count }
+      end
+
+      context 'with given param' do
+        it { expect(CollegeCourse.by_name('by name')).to match_array([college_course]) }
+
+        it { expect(CollegeCourse.by_name('by name').count).to eq 1 }
+      end
+    end
+  end
+
   describe "#attributes" do
     it { is_expected.to respond_to :name }
     it { is_expected.to respond_to :podio_id }
@@ -9,9 +32,5 @@ RSpec.describe CollegeCourse, type: :model do
   describe "#validations" do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :podio_id }
-  end
-
-  describe "#associations" do
-    it { is_expected.to have_many :exchange_participants }
   end
 end
