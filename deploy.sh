f71a650145a44cddc360c09b146b5575c5c10f6f
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2018-08-07 14:15:59 -0300

    fix(travis): Adds TRAVIS_PULL_REQUEST check to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-if [ "$TRAVIS_BRANCH" == "staging" ]; then
+if [ "$TRAVIS_BRANCH" == "staging" ] && [ "$TRAVIS_PULL_REQUEST" == "false"]; then
 	openssl aes-256-cbc -k $DEPLOY_KEY -in config/deploy_id_rsa_travis_enc_travis -d -a -out
 	bundle exec cap staging deploy
 fi
