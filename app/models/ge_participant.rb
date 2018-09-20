06152f2328b13b3039bd140c852ad1ae3c979bfa
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2018-09-20 12:05:10 -0300

    refactor(ge_participant): implements new scholarity logic

--- app/models/ge_participant.rb
+++ app/models/ge_participant.rb
@@ -3,7 +3,7 @@ class GeParticipant < ApplicationRecord
   has_one :english_level, as: :englishable, dependent: :destroy
 
   delegate :as_sqs, :fullname, :cellphone, :email, :birthdate,
-           :first_name, :last_name,
+           :first_name, :last_name, :scholarity,
            to: :exchange_participant, prefix: false
 
   accepts_nested_attributes_for :exchange_participant
