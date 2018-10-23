f12a9d8530833f0b8dac677516394da1db6da5df
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2018-10-23 10:25:09 -0300

    refactor(youth_validator): guard clause add instead of wrapping comparison on conditions

--- app/validators/youth_validator.rb
+++ app/validators/youth_validator.rb
@@ -1,9 +1,8 @@
 class YouthValidator < ActiveModel::Validator
   def validate(record)
-    unless !record.birthdate &&
-           record.birthdate <= 18.years.ago &&
-           record.birthdate >= 30.years.ago
-      record.errors[:birthdate] << 'Necessário ter entre 18 e 30 anos.'
-    end
+    return if record.birthdate && record.birthdate <= 18.years.ago &&
+              record.birthdate >= 30.years.ago
+
+    record.errors[:birthdate] << 'Necessário ter entre 18 e 30 anos.'
   end
 end
