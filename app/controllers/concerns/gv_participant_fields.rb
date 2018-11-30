25fee5ed722fad8f7c445614554c32bdf20fff89
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2018-11-30 20:25:57 -0200

    feat(models/gx_participants): adds podio_id to all programs participants fix(Gemfile): reverts rails down to 5.2.1 because of a bug handling uploads to s3 feat(controllers/gx_participant_fields): adds and corrects fields to synchronize with podio feat(services/send_to_podio): refactors and adds new fields to sync with Argentina feat(config): sets up storage on s3 for uploaded files
    Rails downgrade will be reverted as soon as 5.2.2 stable is released

--- app/controllers/concerns/gv_participant_fields.rb
+++ app/controllers/concerns/gv_participant_fields.rb
@@ -9,6 +9,7 @@ module GvParticipantFields
 
   def gv_participant_fields_bra
     {
+      'exchange_participant_id' => gv_participant.exchange_participant.id,
       'email' => gv_participant.email,
       'fullname' => gv_participant.fullname,
       'cellphone' => gv_participant.cellphone,
@@ -29,6 +30,7 @@ module GvParticipantFields
 
   def gv_participant_fields_arg
     {
+      'exchange_participant_id' => gv_participant.exchange_participant.id,
       'email' => gv_participant.email,
       'fullname' => gv_participant.fullname,
       'cellphone' => gv_participant.cellphone,
@@ -43,8 +45,9 @@ module GvParticipantFields
       'local_committee' => gv_participant.exchange_participant&.local_committee&.podio_id,
       'university' => gv_participant.exchange_participant&.university&.podio_id,
       'college_course' => gv_participant.exchange_participant&.college_course&.podio_id,
-      'when_can_travel' => gv_participant.when_can_travel,
+      'when_can_travel' => gv_participant&.read_attribute_before_type_cast(:when_can_travel),
       'other_university' => gv_participant.exchange_participant&.other_university,
+      'cellphone_contactable' => gv_participant.exchange_participant.cellphone_contactable
     }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_19_200151) do
+ActiveRecord::Schema.define(version: 2018_11_30_200740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,18 +102,21 @@ ActiveRecord::Schema.define(version: 2018_11_19_200151) do
     t.datetime "updated_at", null: false
     t.integer "when_can_travel"
     t.integer "preferred_destination"
+    t.integer "podio_id"
   end
 
   create_table "gt_participants", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "preferred_destination"
+    t.integer "podio_id"
   end
 
   create_table "gv_participants", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "when_can_travel"
+    t.integer "podio_id"
   end
 
   create_table "local_committees", force: :cascade do |t|
