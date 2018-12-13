a9e458dd7f3137d55b916b083567a4ab62d626e1
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2018-12-13 11:11:58 -0200

    fix(expa::application): changes status type to integer and corrects relationship with exchange participant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_172531) do
+ActiveRecord::Schema.define(version: 2018_12_12_181931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,7 +89,7 @@ ActiveRecord::Schema.define(version: 2018_12_11_172531) do
 
   create_table "expa_applications", force: :cascade do |t|
     t.integer "expa_id"
-    t.string "status"
+    t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "exchange_participant_id"
