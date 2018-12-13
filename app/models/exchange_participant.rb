d77e26edd8d39fed27333044fffb926dfa7794ab
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2018-12-13 11:10:41 -0200

    fix(exchange_participant): need to set class name as model is namespaced

--- app/models/exchange_participant.rb
+++ app/models/exchange_participant.rb
@@ -11,7 +11,7 @@ class ExchangeParticipant < ApplicationRecord
   validates :birthdate, presence: true
   validates :password, presence: true
 
-  has_many :expa_applications
+  has_many :expa_applications, class_name: 'Expa::Application'
 
   belongs_to :registerable, polymorphic: true
   belongs_to :campaign, optional: true
