14974008dada1348aa44ceef8de65dbafe3b84ba
Gabriel Fonseca <gabriel@devmagic.com.br>
2018-12-28 16:05:03 -0200

    Tests the use of last updated for sync local applications with Podio

--- app/services/sync_podio_application_status.rb
+++ app/services/sync_podio_application_status.rb
@@ -4,5 +4,17 @@ class SyncPodioApplicationStatus
   end
   def call
     puts 'hello world'
+    last_applications(last_updated)
+  end
+
+  private
+
+  def last_updated
+    SyncParam.first&.updated_at || 3.month.ago.round
+  end
+
+  def last_applications(from)
+    puts 'call last_applications'
+    Expa::Application.where(updated_at: from)
   end
 end
--- spec/services/sync_podio_application_status_spec.rb
+++ spec/services/sync_podio_application_status_spec.rb
@@ -1,8 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe SyncPodioApplicationStatus do
-  subject { described_class.new }
-
   it { expect(described_class).to respond_to(:call) }
-  it { is_expected.to respond_to(:call) }
+  it { expect(described_class.new).to respond_to(:call) }
+
+  describe '#call' do
+    before do
+      # TODO: We should create a Repository pattern to avoid these dysfunctions
+      allow(Expa::Application).to receive(:where)
+      allow(SyncParam).to receive(:first)
+      described_class.call
+    end
+
+    it { expect(SyncParam).to have_received(:first) }
+
+    it {
+      expect(Expa::Application).to have_received(:where)
+        .with(hash_including(updated_at: 3.months.ago.round))
+    }
+  end
 end
