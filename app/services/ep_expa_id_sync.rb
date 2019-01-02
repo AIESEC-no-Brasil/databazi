e2ba1870c3998faa353eeb7b85c4e1f7664ccaaa
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2019-01-02 16:50:12 -0200

    fix(ep_expa_id_sync): fixes logic & specs, also some refactoring done

--- app/services/ep_expa_id_sync.rb
+++ app/services/ep_expa_id_sync.rb
@@ -6,8 +6,8 @@ class EpExpaIdSync
   end
 
   def call(**args)
-    logger = args[:logger] || Logger.new(STDOUT)
-    logger.info 'ep_expa_id_sync.call'
+    @logger = args[:logger] || Logger.new(STDOUT)
+    @logger.info 'ep_expa_id_sync.call'
     exchange_participant = ExchangeParticipant.find_by(expa_id: nil)
     return unless exchange_participant
 
@@ -19,27 +19,27 @@ class EpExpaIdSync
           ExistsQuery,
           variables: { email: @email })&.data&.check_person_present&.id
       
-      id ? successful_sync(exchange_participant) : failed_sync(exchange_participant)
+      id ? successful_sync(exchange_participant, id) : failed_sync(exchange_participant)
 
       exchange_participant.update_attributes(expa_id: id || 0)
     rescue StandardError => e
-      logger.error "Error when sync #{@email} error #{e.to_json}"
-      logger.error "#{e.backtrace.inspect}"
+      @logger.error "Error when sync #{@email} error #{e.to_json}"
+      @logger.error "#{e.backtrace.inspect}"
     end
   end
 
   private
 
-  def successful_sync(exchange_participant)
-    logger.debug("Found EP in expa #{@email} with EXPA_ID: #{id}")
-    logger.debug("Updated EP with EXPA_ID: #{id}")
+  def successful_sync(exchange_participant, id)
+    @logger.debug("Found EP in expa #{@email} with EXPA_ID: #{id}")
+    @logger.debug("Updated EP with EXPA_ID: #{id}")
   end
 
   def failed_sync(exchange_participant)
-    logger.warn("Couldn't find EP in expa #{@email}, retrying sign up")
+    @logger.warn("Couldn't find EP in expa #{@email}, retrying sign up")
     status = ExpaSignUp.call({ exchange_participant_id: exchange_participant.id }.stringify_keys)
     exchange_participant.reload
-    logger.debug("Retry status: #{status}")
-    logger.debug("EP has now EXPA ID: #{exchange_participant.expa_id}")
+    @logger.debug("Retry status: #{status}")
+    @logger.debug("EP has now EXPA ID: #{exchange_participant.expa_id}")
   end
 end
