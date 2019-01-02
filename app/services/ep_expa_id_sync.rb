a21fd8e91477fdea5ba38247086315a78ccd6ec7
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2019-01-02 15:09:04 -0200

    refactor(ep_expa_id_sync): general code cleanup & adds signup retry on failure to fetch expa_id

--- app/services/ep_expa_id_sync.rb
+++ app/services/ep_expa_id_sync.rb
@@ -8,26 +8,36 @@ class EpExpaIdSync
   def call(**args)
     logger = args[:logger] || Logger.new(STDOUT)
     logger.info 'ep_expa_id_sync.call'
-    ep = ExchangeParticipant.find_by(expa_id: nil)
-    return if ep.nil?
+    exchange_participant = ExchangeParticipant.find_by(expa_id: nil)
+    return unless exchange_participant
 
     begin
       id = EXPAAPI::Client
         .query(
           ExistsQuery,
-          variables: { email: ep.email.downcase })&.data&.check_person_present&.id
-      logger.debug("Found ep in expa #{ep.email.downcase} with EXPA_ID: #{id}") unless id.nil?
-      logger.warn("Couldn't find ep in expa #{ep.email.downcase}") if id.nil?
-      id ||= 0
-      # ep.expa_id = id
-      # ep.save(validate: false)
-      ep.update_attributes(expa_id: id)
+          variables: { email: exchange_participant.email.downcase })&.data&.check_person_present&.id
+      
+      id ? successful_sync(exchange_participant) : failed_sync(exchange_participant)
+
+      exchange_participant.update_attributes(expa_id: id || 0)
     rescue StandardError => e
-      logger.error "Error when sync #{ep.email.downcase} erro #{e.to_json}"
+      logger.error "Error when sync #{exchange_participant.email.downcase} error #{e.to_json}"
       logger.error "#{e.backtrace.inspect}"
     end
   end
 
   private
 
+  def successful_sync(exchange_participant)
+    logger.debug("Found EP in expa #{exchange_participant.email.downcase} with EXPA_ID: #{id}")
+    logger.debug("Updated EP with EXPA_ID: #{id}")
+  end
+
+  def failed_sync(exchange_participant)
+    logger.warn("Couldn't find EP in expa #{exchange_participant.email.downcase}, retrying sign up")
+    status = ExpaSignUp.call({ exchange_participant_id: exchange_participant.id }.stringify_keys)
+    exchange_participant.reload
+    logger.debug("Retry status: #{status}")
+    logger.debug("EP has now EXPA ID: #{exchange_participant.expa_id}")
+  end
 end
