6fc1f175630849908c43cc7cb890f77b30da2d77
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2019-01-11 15:04:11 -0200

    fixes expa application specs

--- spec/models/expa/application_spec.rb
+++ spec/models/expa/application_spec.rb
@@ -6,9 +6,11 @@ RSpec.describe Expa::Application, type: :model do
   end
 
   it do
-    expect(Expa::Application.new).to define_enum_for(:status).with(
-      open: 1, applied: 2, accepted: 3, approved: 4,
-      break_approved: 5, rejected: 6, withdrawn: 7, realized: 100,
-      realization_broken: 101, matched: 102, completed: 103)
+    expect(Expa::Application.new).to define_enum_for(:status)
+      .with(open: 1, applied: 2, accepted: 3, approved: 4,
+            break_approved: 5, rejected: 6, withdrawn: 7,
+            realized: 100, approval_broken: 101, realization_broken: 102, matched: 103,
+            completed: 104
+           )
   end
 end
