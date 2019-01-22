30e777d25c3a016cdf6222719bdc51a8790662c4
Diego Dillenburg Bueno <diegodillenburg+github@gmail.com>
2019-01-22 15:09:18 -0200

    Revert "removes graphql"

--- lib/expa_api.rb
+++ lib/expa_api.rb
@@ -1,79 +1,79 @@
-# require 'graphql/client'
-# require 'graphql/client/http'
+require 'graphql/client'
+require 'graphql/client/http'
 
-# module EXPAAPI
-#   def self.access_token
-#     HTTParty
-#       .get("http://token.aiesec.org.br/" \
-#         "get_token.php?token=#{token_token}"
-#       )
-#       .body
-#   end
+module EXPAAPI
+  def self.access_token
+    HTTParty
+      .get("http://token.aiesec.org.br/" \
+        "get_token.php?token=#{token_token}"
+      )
+      .body
+  end
 
-#   def self.token_token
-#     ENV['API_AUTHENTICITY_TOKEN']
-#   end
+  def self.token_token
+    ENV['API_AUTHENTICITY_TOKEN']
+  end
 
-#   HTTP = GraphQL::Client::HTTP.new("https://gis-api.aiesec.org/graphql?access_token=#{access_token}")
-#   Schema = GraphQL::Client.load_schema(HTTP)
+  HTTP = GraphQL::Client::HTTP.new("https://gis-api.aiesec.org/graphql?access_token=#{access_token}")
+  Schema = GraphQL::Client.load_schema(HTTP)
 
-#   Client = GraphQL::Client.new(schema: Schema, execute: HTTP)
-# end
+  Client = GraphQL::Client.new(schema: Schema, execute: HTTP)
+end
 
-# ExistsQuery = EXPAAPI::Client.parse <<~'GRAPHQL'
-#     query($email: String) {
-#       checkPersonPresent(email: $email) {
-#         id
-#         full_name
-#         email
-#     }
-# }
-# GRAPHQL
+ExistsQuery = EXPAAPI::Client.parse <<~'GRAPHQL'
+    query($email: String) {
+      checkPersonPresent(email: $email) {
+        id
+        full_name
+        email
+    }
+}
+GRAPHQL
 
-# LoadApplications = EXPAAPI::Client.parse <<~'GRAPHQL'
-#   query($from: DateTime) {
-#     allOpportunityApplication(
-#       sort: "ASC_updated_at",
-#       filters:{
-#       person_home_mc: 1606,
-#       last_interaction: {from: $from}
-#     }) {
-#       paging {
-#         total_pages
-#       }
-#       data{
-#         id
-#         status
-#         updated_at
-#         host_lc_name
-#         person {
-#           id
-#           full_name
-#           phone
-#           home_mc {
-#             name
-#           }
-#           home_lc {
-#             name
-#           }
-#           user_id
-#           email
-#           secure_identity_email
-#         }
-#       }
-#     }
-#   }
-# GRAPHQL
+LoadApplications = EXPAAPI::Client.parse <<~'GRAPHQL'
+  query($from: DateTime) {
+    allOpportunityApplication(
+      sort: "ASC_updated_at",
+      filters:{
+      person_home_mc: 1606,
+      last_interaction: {from: $from}
+    }) {
+      paging {
+        total_pages
+      }
+      data{
+        id
+        status
+        updated_at
+        host_lc_name
+        person {
+          id
+          full_name
+          phone
+          home_mc {
+            name
+          }
+          home_lc {
+            name
+          }
+          user_id
+          email
+          secure_identity_email
+        }
+      }
+    }
+  }
+GRAPHQL
 
-# CountApplications = EXPAAPI::Client.parse <<~'GRAPHQL'
-#   query($from: DateTime, $to: DateTime) {
-#     allOpportunityApplication(filters:{
-#       person_home_mc: 1606,
-#       last_interaction: {from: $from, to: $to}
-#     }) {
-#       paging {
-#         total_pages
-#       }
-#     }
-#   }
-# GRAPHQL
+CountApplications = EXPAAPI::Client.parse <<~'GRAPHQL'
+  query($from: DateTime, $to: DateTime) {
+    allOpportunityApplication(filters:{
+      person_home_mc: 1606,
+      last_interaction: {from: $from, to: $to}
+    }) {
+      paging {
+        total_pages
+      }
+    }
+  }
+GRAPHQL
