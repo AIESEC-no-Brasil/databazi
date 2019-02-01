bb5b2e6d035b125b0eb9902735fc1f2c1bc42431
Gabriel Fonseca <gabriel@devmagic.com.br>
2019-02-01 18:00:21 -0200

    Add Sentry to process

--- app/services/expa_icx_sync.rb
+++ app/services/expa_icx_sync.rb
@@ -8,14 +8,24 @@ class ExpaIcxSync
     logger.info "Start sync"
 
     RepositoryExpaApi.load_icx_applications(from).each do |application|
-      logger.info "waiting"
-      RepositoryApplication.save_icx_from_expa(application)
-      logger.info "Saved ICX Application into Databazi"
+      begin
+        logger.info "waiting"
+        RepositoryApplication.save_icx_from_expa(application)
+        logger.info "Saved ICX Application into Databazi"
+      rescue => exception
+        Raven.capture_exception(exception)
+        @logger.error exception.message
+      end
     end
 
     RepositoryApplication.pending_podio_sync_icx_applications.each do |application|
-      RepositoryPodio.save_icx_application(application)
-      logger.info "Saved ICX Application into Podio"
+      begin
+        RepositoryPodio.save_icx_application(application)
+        logger.info "Saved ICX Application into Podio"
+      rescue => exception
+        Raven.capture_exception(exception)
+        @logger.error exception.message
+      end
     end
     logger.info "Done sync"
     sleep(2)
