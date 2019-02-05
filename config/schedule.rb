eba5db31f1395449ab49337963d00313971305d0
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2019-02-05 13:51:54 -0200

    fix(schedule): merge conflicts

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,34 +1,13 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
-# Example:
-#
 set :output, path + "/cron_log.log"
 
-every '* * * * *' do
+every 5.minutes do
   runner "ExpaApplicationSync.call", environment: 'production'
 end
 
-# workaround provided so the service is called every 30 seconds
-
-every '* * * * *' do
-  runner "DelayedCall.call({ delay: 30, job: 'ExpaApplicationSync' })", environment: 'production'
-end
-
-every '* * * * *' do
+every 5.minutes do
   runner "ExpaIcxSync.call", environment: 'production'
 end
 
-# workaround provided so the service is called every 30 seconds
-
-every '* * * * *' do
-  runner "DelayedCall.call({ delay: 30, job: 'ExpaIcxSync' })", environment: 'production'
-end
-
-every 1.minute do
+every 7.minutes do
   runner "SyncPodioApplicationStatus.call", environment:'production'
 end
-
-# Learn more: http://github.com/javan/whenever
