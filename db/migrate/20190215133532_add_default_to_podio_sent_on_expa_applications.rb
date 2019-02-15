b024d1e40edbd4a3aa6d1bc0657341c7038bfbc6
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2019-02-15 12:35:21 -0200

    updates hash syntax

--- db/migrate/20190215133532_add_default_to_podio_sent_on_expa_applications.rb
+++ db/migrate/20190215133532_add_default_to_podio_sent_on_expa_applications.rb
@@ -1,5 +1,5 @@
 class AddDefaultToPodioSentOnExpaApplications < ActiveRecord::Migration[5.2]
   def change
-    change_column :expa_applications, :podio_sent, :boolean, :default => false
+    change_column :expa_applications, :podio_sent, :boolean, default: false
   end
 end
