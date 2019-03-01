12d6bb94d821494c0d73251e34ba10470df5633f
Danillo <danillo@devmagic.com.br>
2019-03-01 14:38:17 -0300

    temp - this commit will be deleted from the tree

--- app/services/expa_application_sync.rb
+++ app/services/expa_application_sync.rb
@@ -16,47 +16,7 @@ class ExpaApplicationSync
     applications = applications.sort { |a, b| Time.parse(a.updated_at) <=> Time.parse(b.updated_at) }
     applications.each do |application|
       begin
-        ep = ExchangeParticipant.find_by_expa_id(application.person.id)
-
-        unless ep.nil?
-          ep.expa_applications.where(expa_id: application.id)
-            .first_or_initialize
-            .update_attributes(status: application.status,
-                              expa_ep_id: application.person.id,
-                              updated_at_expa: parsed_date(application.updated_at),
-                              applied_at: parsed_date(application.created_at),
-                              accepted_at: application.status != 'rejected' ? parsed_date(application.matched_or_rejected_at) : nil,
-                              approved_at: parsed_date(application.date_approved),
-                              break_approved_at: application.status == 'rejected' ? parsed_date(application.matched_or_rejected_at) : nil,
-                              realized_at: parsed_date(application.date_realized),
-                              completed_at: parsed_date(application.experience_end_date),
-                              podio_last_sync: nil,
-                              product: application.opportunity.programme.short_name_display.downcase.to_sym,
-                              tnid: application.opportunity.id,
-                              standards: application.standards)
-          ep.update_attributes(status: exchange_participant_status_expa(application.person.status))
-          log = "Sync application with EP #{ep&.fullname}"
-        end
-
-        if ep.nil?
-          Expa::Application.where(expa_id: application.id)
-            .first_or_initialize
-            .update_attributes(status: application.status,
-                              expa_ep_id: application.person.id,
-                              updated_at_expa: parsed_date(application.updated_at),
-                              applied_at: parsed_date(application.created_at),
-                              accepted_at: application.status != 'rejected' ? parsed_date(application.matched_or_rejected_at) : nil,
-                              approved_at: parsed_date(application.date_approved),
-                              break_approved_at: application.status == 'rejected' ? parsed_date(application.matched_or_rejected_at) : nil,
-                              realized_at: parsed_date(application.date_realized),
-                              completed_at: parsed_date(application.experience_end_date),
-                              podio_last_sync: nil,
-                              product: application.opportunity.programme.short_name_display.downcase.to_sym,
-                              tnid: application.opportunity.id,
-                              standards: application.standards)
-
-          log = 'Sync application without EP'
-        end
+        save_ogx_application(application)
         log += " last status #{application.status}"
         log += " application id #{application.id}"
         logger.info log
@@ -73,14 +33,62 @@ class ExpaApplicationSync
     end
   end
 
-  
+  def self.save_ogx_application(application)
+    ep = ExchangeParticipant.find_by_expa_id(application.person.id)
+
+    unless ep.nil?
+      ep.expa_applications.where(expa_id: application.id)
+        .first_or_initialize
+        .update_attributes(
+          status: application.status,
+          expa_ep_id: application.person.id,
+          updated_at_expa: parsed_date(application.updated_at),
+          applied_at: parsed_date(application.created_at),
+          accepted_at: application.status != 'rejected' ? parsed_date(application.matched_or_rejected_at) : nil,
+          approved_at: parsed_date(application.date_approved),
+          break_approved_at: application.status == 'rejected' ? parsed_date(application.matched_or_rejected_at) : nil,
+          realized_at: parsed_date(application.date_realized),
+          completed_at: parsed_date(application.experience_end_date),
+          podio_last_sync: nil,
+          product: application.opportunity.programme.short_name_display.downcase.to_sym,
+          tnid: application.opportunity.id,
+          standards: application.standards
+        )
+      ep.update_attributes(status: exchange_participant_status_expa(application.person.status))
+      log = "Sync application with EP #{ep&.fullname}"
+    end
+
+    if ep.nil?
+      Expa::Application.where(expa_id: application.id)
+        .first_or_initialize 
+        .update_attributes(
+          status: application.status,
+          expa_ep_id: application.person.id,
+          updated_at_expa: parsed_date(application.updated_at),
+          applied_at: parsed_date(application.created_at),
+          accepted_at: application.status != 'rejected' ? parsed_date(application.matched_or_rejected_at) : nil,
+          approved_at: parsed_date(application.date_approved),
+          break_approved_at: application.status == 'rejected' ? parsed_date(application.matched_or_rejected_at) : nil,
+          realized_at: parsed_date(application.date_realized),
+          completed_at: parsed_date(application.experience_end_date),
+          podio_last_sync: nil,
+          product: application.opportunity.programme.short_name_display.downcase.to_sym,
+          tnid: application.opportunity.id,
+          standards: application.standards
+        )
+      log = 'Sync application without EP'
+    end
+    p log
+    true
+  end
+
   private
 
-  def exchange_participant_status_expa(status)
+  def self.exchange_participant_status_expa(status)
     status == "other" ? :other_status : status
   end
 
-  def parsed_date(date)
+  def self.parsed_date(date)
     Time.parse(date) if date
   end
 
