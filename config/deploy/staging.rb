fafc9d40386f1a80afcb536af6f33cfaaa09d64f
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2019-03-25 16:01:34 -0300

    deploy: removes arg address

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,7 +2,7 @@
 server '52.8.184.67', port: 22, user: 'ubuntu', roles: [:web, :app, :db], primary: true
 
 # ARG
-server '13.52.15.180', port: 22, user: 'ubuntu', roles: [:web, :app, :db], primary: true
+# server '13.52.15.180', port: 22, user: 'ubuntu', roles: [:web, :app, :db], primary: true
 
 set :branch, :staging
 
