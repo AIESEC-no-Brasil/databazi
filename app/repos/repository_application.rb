4f3efa6a0fcef9d56abcf736d817f6d116ef47a3
Danillo <danillo@devmagic.com.br>
2019-03-26 15:00:48 -0300

    hotfix(repository_application): fixed typo in orm query

--- app/repos/repository_application.rb
+++ app/repos/repository_application.rb
@@ -40,7 +40,7 @@ class RepositoryApplication
       .where(podio_last_sync: nil)
       .where(has_error: false)
       .order('updated_at_expa': :desc)
-      limit(10)
+      .limit(10)
   end
 
   private
