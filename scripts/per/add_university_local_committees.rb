291bb9c1c3f066f95fac56246aaaeb174b5b75af
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2019-03-31 15:56:46 -0300

    feat(scripts): adds scripts to populate peruvian database

---
+++ scripts/per/add_university_local_committees.rb
@@ -0,0 +1,18 @@
+data = CSV.read("#{Rails.root}/files/uni_lc.csv")
+
+data.shift
+
+data.each do |uni_lc|
+  university = University.find_by(expa_id: uni_lc[0].to_i)
+  university_id = university.try(:id)
+
+  gv_committee_id = LocalCommittee.find_by('unaccent(name) ilike unaccent(?)',  uni_lc[1]).id
+  ge_committee_id = LocalCommittee.find_by('unaccent(name) ilike unaccent(?)',  uni_lc[2]).id
+  gt_committee_id = LocalCommittee.find_by('unaccent(name) ilike unaccent(?)',  uni_lc[3]).id
+
+  if university_id
+    UniversityLocalCommittee.create(university_id: university_id, local_committee_id: gv_committee_id, program: 0)
+    UniversityLocalCommittee.create(university_id: university_id, local_committee_id: ge_committee_id, program: 1)
+    UniversityLocalCommittee.create(university_id: university_id, local_committee_id: gt_committee_id, program: 2)
+  end
+end
