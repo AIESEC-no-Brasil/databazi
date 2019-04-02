6cefb1ae28388198420127218deca15d0bc8b2e8
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2019-04-01 23:30:47 -0300

    feat(gx_participant): adds when_can_travel options for peruvian users

--- app/models/gv_participant.rb
+++ app/models/gv_participant.rb
@@ -1,4 +1,9 @@
 class GvParticipant < ApplicationRecord
+  ARGENTINEAN_WHEN_CAN_TRAVEL = %i[as_soon_as_possible next_three_months
+                                   next_six_months in_one_year]
+
+  PERUVIAN_WHEN_CAN_TRAVEL = %i[as_soon_as_possible next_three_months
+                                next_six_months informational_only]
   has_one :exchange_participant, as: :registerable, dependent: :destroy
 
   delegate :as_sqs, :fullname, :cellphone, :email, :birthdate,
@@ -7,8 +12,17 @@ class GvParticipant < ApplicationRecord
 
   accepts_nested_attributes_for :exchange_participant
 
-  enum when_can_travel: %i[as_soon_as_possible next_three_months
-                           next_six_months in_one_year]
-
   validates :when_can_travel, presence: true, if: :argentina?
+
+  def when_can_travel_sym(when_can_travel)
+    ENV['COUNTRY'] == 'arg' ? argentinean_when_can_travel(when_can_travel) : peruvian_when_can_travel(when_can_travel)
+  end
+
+  def argentinean_when_can_travel(when_can_travel)
+    ExchangeParticipant::ARGENTINEAN_WHEN_CAN_TRAVEL[when_can_travel]
+  end
+
+  def peruvian_when_can_travel(when_can_travel)
+    ExchangeParticipant::PERUVIAN_WHEN_CAN_TRAVEL[when_can_travel]
+  end
 end
