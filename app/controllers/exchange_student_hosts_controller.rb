755904f7608830c0443e66344bbbe353200f107a
Diego Dillenburg Bueno <diegodillenburg+github@gmail.com>
2019-05-07 10:49:39 -0300

    feat(exchange_student_hosts): creates model, controller, service, and worker for exchange_student_hosts endpoint

---
+++ app/controllers/exchange_student_hosts_controller.rb
@@ -0,0 +1,27 @@
+class ExchangeStudentHostsController < ApplicationController
+  expose :exchange_student_host
+
+   def create
+    if exchange_student_host.save
+      perform_on_workers
+      render json: { status: :success }
+    else
+      render json: {
+        status: :failure,
+        messages: exchange_student_host.errors.messages
+      }
+    end
+  end
+
+   private
+
+   def perform_on_workers
+    ExchangeStudentHostWorker.perform_async(exchange_student_host.as_sqs)
+  end
+
+   def exchange_student_host_params
+    params.require(:exchange_student_host).permit(
+      :fullname, :email, :cellphone, :zipcode,
+      :neighborhood, :city, :state, :cellphone_contactable, :local_committee_id)
+  end
+end
---
+++ spec/models/exchange_student_host_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+ RSpec.describe ExchangeStudentHost, type: :model do
+  describe '#attributes' do
+    it { is_expected.to respond_to :fullname }
+    it { is_expected.to respond_to :email }
+    it { is_expected.to respond_to :cellphone }
+    it { is_expected.to respond_to :zipcode }
+    it { is_expected.to respond_to :neighborhood }
+    it { is_expected.to respond_to :city }
+    it { is_expected.to respond_to :state }
+    it { is_expected.to respond_to :cellphone_contactable }
+  end
+
+   describe '#validations' do
+    subject { build(:exchange_student_host) }
+
+     it { is_expected.to validate_presence_of :fullname }
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_presence_of :cellphone }
+    it { is_expected.to validate_presence_of :zipcode }
+    it { is_expected.to validate_presence_of :neighborhood }
+    it { is_expected.to validate_presence_of :city }
+    it { is_expected.to validate_presence_of :state }
+  end
+
+   describe 'methods' do
+    it '#as_sqs format' do
+      exchange_student_host = create(:exchange_student_host)
+      as_sqs = { exchange_student_host_id: exchange_student_host.id }
+
+       expect(exchange_student_host.as_sqs).to eq as_sqs
+    end
+  end
+end
