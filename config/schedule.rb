6bbd28fe7b53cc54727deb5189161fc606da726c
Diego Dillenburg Bueno <diegodillenburg+github@gmail.com>
2019-07-30 13:25:40 -0300

    fix(schedule): adds country verification

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,17 +1,21 @@
 set :output, path + "/cron_log.log"
 
-every 5.minutes do
-  runner "ExpaPeopleSync.call", environment: 'production'
+if ENV['COUNTRY'] == 'per'
+  every 5.minutes do
+    runner "ExpaPeopleSync.call", environment: 'production'
+  end
 end
 
-# every 5.minutes do
-#   runner "ExpaApplicationSync.call", environment: 'production'
-# end
+if ENV['COUNTRY'] == 'bra'
+  every 5.minutes do
+    runner "ExpaApplicationSync.call", environment: 'production'
+  end
 
-# every 5.minutes do
-#   runner "ExpaIcxSync.call", environment: 'production'
-# end
+  every 5.minutes do
+    runner "ExpaIcxSync.call", environment: 'production'
+  end
 
-# every 7.minutes do
-#   runner "SyncPodioApplicationStatus.call", environment:'production'
-# end
+  every 7.minutes do
+    runner "SyncPodioApplicationStatus.call", environment:'production'
+  end
+end
