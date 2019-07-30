db440877e2e98bdaf7ed4e6e7c5f5313aaf54dac
Diego Dillenburg Bueno <diegodillenburg+github@gmail.com>
2019-07-30 10:51:17 -0300

    feat(schedule): adds expa_people_sync to crontab

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,13 +1,17 @@
 set :output, path + "/cron_log.log"
 
 every 5.minutes do
-  runner "ExpaApplicationSync.call", environment: 'production'
+  runner "ExpaPeopleSync.call", environment: 'production'
 end
 
-every 5.minutes do
-  runner "ExpaIcxSync.call", environment: 'production'
-end
+# every 5.minutes do
+#   runner "ExpaApplicationSync.call", environment: 'production'
+# end
 
-every 7.minutes do
-  runner "SyncPodioApplicationStatus.call", environment:'production'
-end
+# every 5.minutes do
+#   runner "ExpaIcxSync.call", environment: 'production'
+# end
+
+# every 7.minutes do
+#   runner "SyncPodioApplicationStatus.call", environment:'production'
+# end
