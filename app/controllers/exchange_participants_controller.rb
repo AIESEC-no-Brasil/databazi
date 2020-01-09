2e15e1e6d0fb3806a61764a8f230f7619929a662
Diego Dillenburg Bueno <diegodillenburg+github@gmail.com>
2020-01-09 11:43:38 -0300

    fix(expa/podio): enhances checking so it does not duplicate EPs incoming from EXPA

--- app/controllers/exchange_participants_controller.rb
+++ app/controllers/exchange_participants_controller.rb
@@ -1,3 +1,5 @@
+require "#{Rails.root}/lib/expa_api"
+
 class ExchangeParticipantsController < ApplicationController
   require 'net/http'
   require 'uri'
@@ -34,19 +36,22 @@ class ExchangeParticipantsController < ApplicationController
         if res.programmes.any?
           res.programmes.each do |programme|
             programme = programme[:short_name_display] if programme[:short_name_display].in?(%w[GV GE GT])
+          end
         end
 
         programme ||= 'GV'
 
-        exchange_participant = ExchangeParticipant.create(
+        exchange_participant = ExchangeParticipant.where(expa_id: res.id).first_or_initialize(
           expa_id: res.id,
           fullname: res.full_name,
-          birthdate: res.dob,
+          birthdate: res.try(:dob),
           email: res.email,
-          local_committee_id: LocalCommittee.where('name ilike ?', res.home_lc.name).first.id,
+          local_committee_id: LocalCommittee.where('name ilike ?', res.home_lc.name).first.try(:id),
           program: programme.downcase.to_sym,
           status: res.status
-        ).validate(:false)
+        )
+
+        exchange_participant.save(validate: false)
       end
 
       ExpaToPodioWorker.perform_async({ 'exchange_participant_id' => exchange_participant.id })
