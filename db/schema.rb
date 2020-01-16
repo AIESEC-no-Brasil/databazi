392b3ef22e9e98b22972b187ecbe7e5296e6d103
Diego Dillenburg Bueno <diegodillenburg+github@gmail.com>
2020-01-16 10:00:40 -0300

    feat(schema): updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_08_175014) do
+ActiveRecord::Schema.define(version: 2020_01_16_124648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 2020_01_08_175014) do
     t.integer "referral_type"
     t.datetime "deleted_at"
     t.integer "program"
+    t.integer "signup_source", default: 0
     t.index ["college_course_id"], name: "index_exchange_participants_on_college_course_id"
     t.index ["local_committee_id"], name: "index_exchange_participants_on_local_committee_id"
     t.index ["registerable_type", "registerable_id"], name: "registerable_index_on_exchange_participants"
@@ -231,6 +232,7 @@ ActiveRecord::Schema.define(version: 2020_01_08_175014) do
     t.bigint "local_committee_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "nearest_committee"
     t.index ["college_course_id"], name: "index_memberships_on_college_course_id"
     t.index ["local_committee_id"], name: "index_memberships_on_local_committee_id"
   end
