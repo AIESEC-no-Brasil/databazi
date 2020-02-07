939dfdd233f7aec468969ba03595d8329cc7c208
Diego Dillenburg Bueno <diegodillenburg+github@gmail.com>
2020-02-07 12:55:25 -0300

    feat(migrations): removes default values from exchange_participants new fields

--- db/migrate/20191030025642_add_has_error_to_exchange_participant.rb
+++ db/migrate/20191030025642_add_has_error_to_exchange_participant.rb
@@ -1,5 +1,5 @@
 class AddHasErrorToExchangeParticipant < ActiveRecord::Migration[5.2]
   def change
-    add_column :exchange_participants, :has_error, :boolean, default: false
+    add_column :exchange_participants, :has_error, :boolean
   end
 end
--- db/migrate/20191031230609_add_expa_id_sync_to_exchange_participants.rb
+++ db/migrate/20191031230609_add_expa_id_sync_to_exchange_participants.rb
@@ -1,5 +1,5 @@
 class AddExpaIdSyncToExchangeParticipants < ActiveRecord::Migration[5.2]
   def change
-    add_column :exchange_participants, :expa_id_sync, :boolean, default: true
+    add_column :exchange_participants, :expa_id_sync, :boolean
   end
 end
